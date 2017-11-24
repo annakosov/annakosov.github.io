be4364b05b2f330d72840bc0ae1f1f11ed25541f
annakosov <31862122+annakosov@users.noreply.github.com>
2017-11-24 14:28:56 -0500

    update

--- contact.php
+++ contact.php
@@ -3,8 +3,8 @@
 require('recaptcha-master/src/autoload.php');
 
 // configure
-$from = 'contact@annakosov.net';
-$sendTo = 'contact@annakosov.net';
+$from = 'anna.kosov@gmail.com';
+$sendTo = 'anna.kosov@gmail.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
